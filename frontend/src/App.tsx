933e8800e6ae2322e5b686af1fe09dcd010ba8e5
VCarmoLima <viniciuscarmo.contato@gmail.com>
2026-03-11 21:57:32 -0300

    chore: inicializa frontend com vite, typescript e dependencias do mui

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { CssBaseline } from '@mui/material';
+
+export default function App() {
+  return (
+    <>
+      <CssBaseline />
+      <div>
+        <h1>Nosso App de Agendamento</h1>
+      </div>
+    </>
+  );
+}
