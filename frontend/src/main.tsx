b8cd1d4532535a07fda686bdaa2ea3d27f8c13bc
VCarmoLima <viniciuscarmo.contato@gmail.com>
2026-03-12 22:12:49 -0300

    chore: configura cliente axios, variaveis de ambiente e inicializa o motor do react query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,25 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ThemeProvider } from '@mui/material/styles';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { theme } from './theme.ts';
 import App from './App.tsx';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+    },
+  },
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>,
 );
